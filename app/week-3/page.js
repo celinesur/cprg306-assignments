ce57cf9881f910a537b90ec6d1fadd4a4f7b04b2
celinesur <ciiellasurjanto@gmail.com>
2025-09-26 16:36:15 -0600

    changed the styling to center the list

--- app/week-3/page.js
+++ app/week-3/page.js
@@ -2,9 +2,11 @@ import ItemList from "./item-list";
 
 export default function Page () {
   return (
-    <main className="p-6 bg-black text-white min-h-screen">
-      <h1 className="text-2xl font-bold mb-6">Shopping List</h1>
-      <ItemList />
+    <main className="p-6 bg-black text-white flex justify-center">
+      <div className="bg-black w-full max-w-md">
+        <h1 className="text-2xl font-bold mb-6">Shopping List</h1>
+        <ItemList />
+      </div>
     </main>
   );
 }
