9ab0c5bd5ad282f95439d0f34b53eadc1cff5c9f
celinesur <ciiellasurjanto@gmail.com>
2025-10-01 11:29:06 -0600

    page.js sets up week 4 page layout and displays the new item component. new-item.js file defines newitem component, and adds the function to increase and decrease the quantity

---
+++ app/week-4/page.js
@@ -0,0 +1,10 @@
+import NewItem from "./new-item";
+
+export default function Page() {
+  return (
+    <main className="p-6 bg-black text-white min-h-screen">
+      <h1 className="text-2xl font-bold mb-6 text-center">Week 4: New Item</h1>
+      <NewItem />
+    </main>
+  );
+}
