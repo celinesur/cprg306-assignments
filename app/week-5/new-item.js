b7fd764bc2ebdf3c3a82fb53ed2235700aed4c05
celinesur <ciiellasurjanto@gmail.com>
2025-10-13 23:08:10 -0600

    created 2 new state variables called name and category

--- app/week-5/new-item.js
+++ app/week-5/new-item.js
@@ -3,7 +3,9 @@
 import { useState } from "react";
 
 export default function NewItem() {
+  const [name, setName ] = useState("");
   const [quantity, setQuantity] = useState(1);
+  const [category, setCategory] = useState("produce");
 
   const increment = () => {
     if (quantity < 20) {
