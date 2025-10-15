4c1d3206515a0efe7436bda3335ac513e5eddaf9
celinesur <ciiellasurjanto@gmail.com>
2025-10-14 19:07:35 -0600

    changed the background to pink gradient

--- app/week-6/page.js
+++ app/week-6/page.js
@@ -2,11 +2,9 @@ import ItemList from "./item-list";
 
 export default function Page () {
   return (
-    <main className="p-6 bg-black text-white flex justify-center">
-      <div className="bg-black w-full max-w-md">
-        <h1 className="text-2xl font-bold mb-6">Shopping List</h1>
-        <ItemList />
-      </div>
+    <main className="p-6 min-h-screen bg-gradient-to-r from-pink-200 via-pink-300 to-pink-400">
+      <h1 className="text-3xl font-bold mb-6 text-center">Shopping List</h1>
+      <ItemList />
     </main>
   );
 }
