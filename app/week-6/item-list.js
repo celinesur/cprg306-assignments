ac0fc3dfc4806d31bb7748918553e89b6df55e04
celinesur <ciiellasurjanto@gmail.com>
2025-10-14 18:29:41 -0600

    sorted the items based on sortBy state variable

--- app/week-6/item-list.js
+++ app/week-6/item-list.js
@@ -4,5 +4,18 @@ import { useState } from "react";
 import data from "./items.json";
 
 export default function ItemList() {
+  // use state for sorting
   const [sortBy, setSortBy] = useState("name");
+
+  // sort items based on sortBy
+  const sortedItems = [...data].sort =((a, b) => {
+    if (sortBy === "name") {
+      return a.name.localeCompare(b.name);
+    } else if (sortBy === "category") {
+      return a.category.localeCompare(b.category);
+    }
+  });
+
+  // render
+  
 }
