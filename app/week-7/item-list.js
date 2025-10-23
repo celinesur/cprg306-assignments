85aca2dca75e9684d24687623c00498c7b203d01
celinesur <ciiellasurjanto@gmail.com>
2025-10-23 15:14:21 -0600

    removed items.json and replaced the functions so it accepts the items as props and sorts them then displays

--- app/week-7/item-list.js
+++ app/week-7/item-list.js
@@ -1,20 +1,17 @@
 "use client";
 import Item from "./item";
 import { useState } from "react";
-import data from "./items.json";
 
-export default function ItemList() {
+export default function ItemList({ items }) {
   // use state for sorting
   const [sortBy, setSortBy] = useState("name");
 
   // sort items based on sortBy
-  const sortedItems = [...data].sort ((a, b) => {
-    if (sortBy === "name") {
-      return a.name.localeCompare(b.name);
-    } else if (sortBy === "category") {
-      return a.category.localeCompare(b.category);
-    }
-  });
+  const sortedItems = [...items].sort ((a, b) =>
+    sortBy === "name"
+      ? a.name.localeCompare(b.name)
+      : a.category.localeCompare(b.category)
+  );
 
   // render
   return (
