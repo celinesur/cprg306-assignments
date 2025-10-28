3ac44e7a44dfa6c96311e00db7e37bd8cd6c379a
celinesur <ciiellasurjanto@gmail.com>
2025-10-27 20:31:04 -0600

    function to fetch the information from the mealdb api

--- app/week-8/meal-ideas.js
+++ app/week-8/meal-ideas.js
@@ -3,6 +3,19 @@
 import { useState } from "react";
 import { useEffect } from "react";
 
+async function fetchMealIdeas(ingredient) {
+  try {
+    const response = await fetch(
+      `https://www.themealdb.com/api/json/v1/1/filter.php?i=${ingredient}`
+    );
+    const data = await response.json();
+    return data.meals || [];
+  } catch (error) {
+    console.error("Error fetching meal ideas:", error);
+    return[];
+  }
+}
+
 export default function MealIdeas({ ingredient}) {
   const [meals, setMeals] = useState([]);
 }
