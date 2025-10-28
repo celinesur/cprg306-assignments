4a83362a69f2bdbb34c9f10829805774efda7d91
celinesur <ciiellasurjanto@gmail.com>
2025-10-27 21:14:47 -0600

    imported meal ideas from meal-ideas

--- app/week-8/page.js
+++ app/week-8/page.js
@@ -4,6 +4,7 @@ import { useState } from "react";
 import ItemList from "./item-list";
 import NewItem from "./new-item";
 import itemsData from "./items.json";
+import MealIdeas from "./meal-ideas";
 
 export default function Page () {
   const [items, setItems] = useState(itemsData);
