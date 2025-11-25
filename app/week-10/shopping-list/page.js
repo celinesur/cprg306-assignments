f93f42a7a70da4e79c3490f8e70471f5dcdc31c1
celinesur <ciiellasurjanto@gmail.com>
2025-11-24 23:19:46 -0700

    removed the hard data file, and updated the appropriate files and functions/created loaditems functions

--- app/week-10/shopping-list/page.js
+++ app/week-10/shopping-list/page.js
@@ -1,16 +1,26 @@
 "use client";
 
 import { useUserAuth } from "../../contexts/AuthContext";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import ItemList from "./item-list";
 import NewItem from "./new-item";
-import itemsData from "./items.json";
 import MealIdeas from "./meal-ideas";
+import { getItems, addItem } from "../_services/shopping-list-service";
 
 export default function Page () {
-  const [items, setItems] = useState(itemsData);
+  const [items, setItems] = useState([]);
   const [selectedItemName, setSelectedItemName] = useState("");
   const {user} = useUserAuth();
+
+  useEffect(() => {
+    async function loadItems() {
+      if (!user) return;
+      const loadedItems = await getItems(user.uid);
+      setItems(loadedItems);
+    }
+
+    loadItems();
+  }, [user]);
 
   if (!user) {
     return (
@@ -19,7 +29,7 @@ export default function Page () {
           You must be logged in to view this page.
         </p>
         <a
-          href="/week-9"
+          href="/week-10"
           className="bg-rose-400 text-white px-4 py-2 rounded hover:bg-rose-500"
           >
             Go back to login
@@ -28,7 +38,9 @@ export default function Page () {
     );
   }
 
-  const handleAddItem = (newItem) => {
+  const handleAddItem = async (newItem) => {
+    const id = await addItem(user.uid, newItem);
+    newItem.id = id;
     setItems((prevItems) => [newItem, ...prevItems]);
   };
 
